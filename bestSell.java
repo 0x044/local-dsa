a91eda663c2ada6075f5b0cf7fe721b37d5e419d
Dharun <dharun.bs2003@gmail.com>
2026-05-26 10:16:06 +0530

    21/05/2026

---
+++ bestSell.java
@@ -0,0 +1,23 @@
+public class bestSell {
+    static void main() {
+        int[] arr = {7,6,4,3,1};
+
+        int minInd = 0;
+        int maxDiff = 0;
+
+        for(int i = 1; i < arr.length; i++){
+            int diff = arr[i] - arr[minInd];
+            maxDiff = Math.max(diff, maxDiff);
+
+            if(arr[minInd] > arr[i]){
+                minInd = i;
+            }
+       }
+
+        if(maxDiff < 0){
+            IO.println(0);
+        }else {
+            IO.println(maxDiff);
+        }
+    }
+}
